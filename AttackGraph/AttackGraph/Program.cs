4736a8f64b4e68d6a7fe573d65a66f756bdbb182
dong0127 <37062581+dong0127@users.noreply.github.com>
2019-01-13 21:43:18 +0800

    iii

--- AttackGraph/AttackGraph/Program.cs
+++ AttackGraph/AttackGraph/Program.cs
@@ -8,20 +8,9 @@ namespace AttackGraph
 
         static void Main(string[] args)
         {
-            //用户输入的初始条件
-            Dictionary<string, int> pre = new Dictionary<string, int>();
-            int[,] connect = new int[3, 3] { { 0, 1, 1 }, { 1, 0, 1 }, { 1, 1, 0 } };
-            int source = 0;
-            int finalTarget = 2;
-            pre.Add("ftp", 1);
-            pre.Add("sshd", 1);
-            pre.Add("ftp", 2);
-            pre.Add("user", source);
-            Graph graph = new Graph();
-            graph.Source = source;
-            graph.Target = finalTarget;
-            graph.Connection = connect;
-            Console.WriteLine(connect[1,1]);
+            
+          
+            Console.WriteLine();
             Console.ReadKey();
         }
     }
