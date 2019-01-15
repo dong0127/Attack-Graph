c1685405f0bc07ba3fdc8a595ef764f74fd916dd
董浩 <sdgsdrgg@hotmail.com>
2019-01-15 22:30:45 +0800

    遍历字典

---
+++ AttackGraph/AttackGraph/Input.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AttackGraph
+{
+    class Precondition: Model
+    {
+         List<string> preconditions = new List<string>()
+        {
+            "user" + "." +0,
+            "ftp" + "." +1,
+            "ftp" + "." +2,
+            "sshd" + "." +1
+        };
+
+        public List<string> Preconditions { get => preconditions; set => preconditions = value; }
+    }
+}
