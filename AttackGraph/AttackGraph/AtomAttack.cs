b56b1a44f11d1f6d80149b73b0d47ad997977968
dong0127 <37062581+dong0127@users.noreply.github.com>
2019-01-17 15:42:03 +0800

    老师作品

--- AttackGraph/AttackGraph/AtomAttack.cs
+++ AttackGraph/AttackGraph/AtomAttack.cs
@@ -5,9 +5,29 @@ using System.Text;
 namespace AttackGraph
 {
 
-    class AtomAttack
+    class AtomAttackTemplate
     {
-        
+        List<Element> preconditions = new List<Element>();
+        List<Element> effects = new List<Element>();
+        string attackname;
+
+        public AtomAttack(List<Element> preconditions, List<Element> effects, string attackname)
+        {
+            this.preconditions = preconditions;
+            this.effects = effects;
+            this.attackname = attackname;
+        }
+
+        getFullAttack(Element ele)
+        {
+
+            return List<Element>
+        }
+
+        getResult()
+        {
+        }
+        /*
         List<List<string>> precondition =new  List<List<string>>()
         {
            new List<string> {"sshd-bof", "user", "sshd" },
@@ -23,8 +43,9 @@ namespace AttackGraph
            new List<string> {"rsh", "user" },
            new List<string>{"local-bof", "root" }
         };
-
+        
         public List<List<string>> Precondition { get => precondition; set => precondition = value; }
         public List<List<string>> Effect { get => effect; set => effect = value; }
+        */
     }
 }
--- AttackGraph/AttackGraph/Program.cs
+++ AttackGraph/AttackGraph/Program.cs
@@ -9,8 +9,14 @@ namespace AttackGraph
         
         static void Main(string[] args)
         {
-        
-            
+
+            List<Element> initial = new List<Element>();
+            List<Element> pre = new List<Element>();
+            List<Element> eff = new List<Element>();
+            pre.Add(new Element("ftp", "x", "y"));
+            pre.Add(new Element("user", "x", "x"));
+            eff.Add(new Element("trust", "y", "x"));
+            AtomAttack atomAttack = new AtomAttack(pre,eff,"Ftp-rhosts");
             Attack attack = new Attack();
             attack.IsAttack();
             Console.WriteLine(attack.Source);
