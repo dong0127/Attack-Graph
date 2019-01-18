4089768ddbe4c4d8b23f42552c5c7a3c881d278e
dong0127 <37062581+dong0127@users.noreply.github.com>
2019-01-18 15:09:59 +0800

    更改后

---
+++ AttackGraph/AttackGraph/AtomAttackTemplate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AttackGraph
+{
+
+    class AtomAttackTemplate
+    {
+        List<Element> preconditions = new List<Element>();
+        List<Element> effects = new List<Element>();
+        string attackname;
+
+        public AtomAttack(List<Element> preconditions, List<Element> effects, string attackname)
+        {
+            this.preconditions = preconditions;
+            this.effects = effects;
+            this.attackname = attackname;
+        }
+
+        getFullAttack(Element ele)
+        {
+
+            return List<Element>
+        }
+
+        getResult()
+        {
+        }
+        /*
+        List<List<string>> precondition =new  List<List<string>>()
+        {
+           new List<string> {"sshd-bof", "user", "sshd" },
+           new List<string> {"Ftp-rhosts", "user", "ftp" },
+           new List<string> {"rsh","user", "trust" },
+           new List<string> {"local-bof", "user" }
+        };
+
+        List<List<string>> effect = new List<List<string>>()
+        {
+           new List<string>{"sshd-bof", "user" },
+           new List<string> {"Ftp-rhosts", "trust" },
+           new List<string> {"rsh", "user" },
+           new List<string>{"local-bof", "root" }
+        };
+        
+        public List<List<string>> Precondition { get => precondition; set => precondition = value; }
+        public List<List<string>> Effect { get => effect; set => effect = value; }
+        */
+    }
+}
---
+++ AttackGraph/AttackGraph/Element.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AttackGraph
+{
+    class Element
+    {
+        string name;
+        string from, to;
+        string type;
+
+        public Element(string name, string from, string to, string type) : this(name, from, to)
+        {
+            this.type = type;
+        }
+
+        public Element(string name, string from, string to)
+        {
+            this.name = name;
+            this.from = from;
+            this.to = to;
+            
+        }
+
+        public string getId()
+        {
+            return null;
+        }
+    }
+}
