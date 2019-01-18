10ff54a2d7e052bf40dcbcfbdac14dfb4775bcb1
dong0127 <37062581+dong0127@users.noreply.github.com>
2019-01-18 21:45:18 +0800

    p

---
+++ AttackGraph/AttackGraph/Attacks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AttackGraph
+{
+    class Attacks
+    {
+        List<AtomAttackTemplate> attack;
+
+        public Attacks(List<AtomAttackTemplate> attacks)
+        {
+            this.Attack = attack;
+        }
+
+        public List<AtomAttackTemplate> Attack { get => attack; set => attack = value; }
+    }
+}
